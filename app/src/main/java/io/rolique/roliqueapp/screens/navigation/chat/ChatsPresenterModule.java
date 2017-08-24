2f772599455c1e2fda264986372ce8c4a70880fa
Volodymyr <volodymyr.oleshkevych@rolique.io>
2017-08-24 12:50:01 +0300

    [+] Added chats module

---
+++ app/src/main/java/io/rolique/roliqueapp/screens/navigation/chat/ChatsPresenterModule.java
@@ -0,0 +1,26 @@
+package io.rolique.roliqueapp.screens.navigation.chat;
+
+import dagger.Module;
+import dagger.Provides;
+import io.rolique.roliqueapp.screens.ViewScope;
+
+/**
+ * Created by Volodymyr Oleshkevych on 8/16/2017.
+ * Copyright (c) 2017, Rolique. All rights reserved.
+ */
+
+@Module
+class ChatsPresenterModule {
+
+    private final ChatsContract.View mView;
+
+    ChatsPresenterModule(ChatsContract.View view) {
+        mView = view;
+    }
+
+    @Provides
+    @ViewScope
+    ChatsContract.View provideMainContractView() {
+        return mView;
+    }
+}
