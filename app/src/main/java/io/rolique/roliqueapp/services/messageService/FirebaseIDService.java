f2d60e78c91b5582c82820e4be66256ea3e40072
oleshkevych <restling1989>
2017-11-28 11:23:07 +0200

    [+] Add services to handle push notification

---
+++ app/src/main/java/io/rolique/roliqueapp/services/messageService/FirebaseIDService.java
@@ -0,0 +1,8 @@
+package io.rolique.roliqueapp.services.messageService;
+
+/**
+ * Created by Volodymyr Oleshkevych on 27.11.17.
+ * Copyright (c) 2017, Rolique. All rights reserved.
+ */
+public class FirebaseIDService {
+}
